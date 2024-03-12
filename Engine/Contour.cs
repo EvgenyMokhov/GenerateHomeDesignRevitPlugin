9133d9e99eb84d8b19f64b98b00907d477ba7fba
unknown <wolfmans.empire@gmail.com>
2024-03-13 04:55:18 +0500

    first commit

---
+++ Engine/Contour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitPlugin
+{
+    public class ContourFlat
+    {
+        public IGeometricShape GeometricShape { get; }
+        public Side SideWithDoor { get; set; }
+        public Side SideWithWindow { get; set; }
+        public string Name { get; }
+
+        public ContourFlat(IGeometricShape geometricShape, Side sideWithDoor, Side sideWithWindow, string name = "")
+        {
+            GeometricShape = geometricShape;
+            SideWithDoor = sideWithDoor;
+            SideWithWindow = sideWithWindow;
+            Name = name;
+        }
+
+    }
+
+}
